4ffd39248f13df79c654caf4940ad62c46ac72c6
unknown <andr3w.sc0tt86@gmail.com>
2020-03-03 13:32:54 -0700

    calculator works BRUH!

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
-var x = document.getElementById("test-id");
-x.textContent = "HEY YOU TOO!";
-console.log(x);
+// var x = document.getElementById("test-id");
+// x.textContent = "HEY YOU TOO!";
+// console.log(x);
 
-var y = document.querySelector(".test-class");
-y.textContent += ", NOT!!";
-console.log(y);
+// var y = document.querySelector(".test-class");
+// y.textContent += ", NOT!!";
+// console.log(y);
 
-var newElement = document.createElement('p');
-newElement.textContent = 'This is a new Element BROOOOOO!';
-document.body.appendChild( newElement );
+// var newElement = document.createElement('p');
+// newElement.textContent = 'This is a new Element BROOOOOO!';
+// document.body.appendChild( newElement );
 
-var newElement2 = document.createElement('p');
-newElement2.textContent = "Second new paragraph";
-newElement2.style.color = 'darkgrey';
+// var newElement2 = document.createElement('p');
+// newElement2.textContent = "Second new paragraph";
+// newElement2.style.color = 'darkgrey';
 
-document.body.appendChild(newElement2);
+// document.body.appendChild(newElement2);
 
-var newElement3 = document.createElement('A');
-newElement3.textContent = "Click Me, Click Me!!";
-newElement3.href = "./index.html";
-newElement3.title = "Return to Homepage";
-newElement3.style.color = "red";
+// var newElement3 = document.createElement('A');
+// newElement3.textContent = "Click Me, Click Me!!";
+// newElement3.href = "./index.html";
+// newElement3.title = "Return to Homepage";
+// newElement3.style.color = "red";
 
-newElement2.appendChild(newElement3);
+// newElement2.appendChild(newElement3);
+
+var calculatorForm = document.getElementById("calculator");
+
+calculatorForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    var operatorInput = document.getElementById('operator');
+    var add1 = document.getElementById("num1");
+    var add2 = document.getElementById("num2");
+    var answer = document.getElementById("answer");
+    if (operatorInput.value == "add")
+    {
+        answer.textContent += Number(add1.value)+Number(add2.value);
+    }
+});
+
+
+
