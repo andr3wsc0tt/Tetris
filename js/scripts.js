dcf19a58628bffeaebdb67eb774508c5092c45ef
unknown <andr3w.sc0tt86@gmail.com>
2020-03-03 11:58:52 -0700

    adding DOM elements with js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-var myHotel = 
-{
-    name: 'The Quay',
-    totalRooms: 40,
-    bookedRooms: 25,
-    types: [
-        'twin',
-        'double',
-        'suite'
-    ],
-    checkAvailability: function() 
-    {
-        console.log('Remaining Rooms: ' + (this.totalRooms - this.bookedRooms));
-    }
-}
+var x = document.getElementById("test-id");
+x.textContent = "HEY YOU TOO!";
+console.log(x);
 
-myHotel.bookedRooms = 30;
-myHotel.checkAvailability();
+var y = document.querySelector(".test-class");
+y.textContent += ", NOT!!";
+console.log(y);
+
+var newElement = document.createElement('p');
+newElement.textContent = 'This is a new Element BROOOOOO!';
+document.body.appendChild( newElement );
+
+var newElement2 = document.createElement('p');
+newElement2.textContent = "Second new paragraph";
+newElement2.style.color = 'darkgrey';
+
+document.body.appendChild(newElement2);
+
+var newElement3 = document.createElement('A');
+newElement3.textContent = "Click Me, Click Me!!";
+newElement3.href = "./index.html";
+newElement3.title = "Return to Homepage";
+newElement3.style.color = "red";
+
+newElement2.appendChild(newElement3);
