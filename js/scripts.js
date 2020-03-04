26d83997bed0f15f8da48e6a4ebf3ec27ab69c85
unknown <andr3w.sc0tt86@gmail.com>
2020-03-04 08:39:30 -0700

    set up canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-var formElement = document.getElementById('form-id');
+const canvas = document.getElementById('Tetris-Canvas');
+const ctx = canvas.getContext('2d');
 
-formElement.addEventListener('submit', function(event)
-{
-
-    event.preventDefault();
-
-    var name = document.getElementById('name');
-    var age = document.getElementById('age');
-    var school_class = document.getElementById('school_class');
-    var city = document.getElementById('city');
-
-    var tableElement = document.getElementById('table-id').getElementsByTagName('tbody')[0];
-    
-    var tableRow = tableElement.insertRow();
-    var tableData1 = tableRow.insertCell();
-    tableData1.textContent = name.value;
-    var tableData2 = tableRow.insertCell();
-    tableData2.textContent = age.value;
-    var tableData3 = tableRow.insertCell();
-    tableData3.textContent = school_class.value;
-    var tableData4 = tableRow.insertCell();
-    tableData4.textContent = city.value;
-
-
-
-}
-);
