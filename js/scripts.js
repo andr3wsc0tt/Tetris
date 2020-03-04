1ae22a4daa96347061707bec8db31818eb8a833c
unknown <andr3w.sc0tt86@gmail.com>
2020-03-04 09:36:17 -0700

    blue block moves around freely

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,81 @@
-const canvas = document.getElementById('Tetris-Canvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('Tetris-Canvas');
+var ctx = canvas.getContext('2d');
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+function keyDownHandler(e)
+{
+    if (e.key == "Right" || e.key  == "ArrowRight")
+    {
+        rightPressed = true;
+    }
+    if (e.key == "Left" || e.key  == "ArrowLeft")
+    {
+        leftPressed = true;
+    }
+    if (e.key == "Up" || e.key  == "ArrowUp")
+    {
+        upPressed = true;
+    }
+    if (e.key == "Down" || e.key  == "ArrowDown")
+    {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e)
+{
+    if (e.key == "Right" || e.key  == "ArrowRight")
+    {
+        rightPressed = false;
+    }
+    if (e.key == "Left" || e.key  == "ArrowLeft")
+    {
+        leftPressed = false;
+    }
+    if (e.key == "Up" || e.key  == "ArrowUp")
+    {
+        upPressed = false;
+    }
+    if (e.key == "Down" || e.key  == "ArrowDown")
+    {
+        downPressed = false;
+    }
+}
+
+function drawBrick()
+{
+    ctx.beginPath();
+    ctx.rect(x, y, 5, 5);
+    ctx.fillStyle = "#0095DD";
+    // var gradient = ctx.createLinearGradient(0, 0, 170, 0);
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBrick();
+
+    if (rightPressed)
+        x += 1;
+    if (leftPressed)
+        x -= 1;
+    if (upPressed)
+        y -= 1;
+    if (downPressed)
+        y += 1;
+ 
+    requestAnimationFrame(draw);
+}
+
+draw();
