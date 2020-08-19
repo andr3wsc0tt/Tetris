11c155ae90bf372e43a5709778cb01479ddd6d5b
Your Name <you@example.com>
2020-08-19 06:13:32 -0600

    fixed

--- Get-The-Century.js
+++
@@ -1,5 +0,0 @@
-function century(year) {
-	var year = Math.floor((year-1)/100) ;
-	var suffix = year+1 > 20 ? 'st' : 'th';
-	return `${year+1}${suffix} century`;
-}
--- Remove-Trailing-and-Leading-Zeros.js
+++
@@ -1,3 +0,0 @@
-function removeLeadingTrailing(n) {
-	return n.replace(/(^0+(?=[0-9])|(\.0*$)|((?<=\.\d*)0+$))/g, "");
-}
